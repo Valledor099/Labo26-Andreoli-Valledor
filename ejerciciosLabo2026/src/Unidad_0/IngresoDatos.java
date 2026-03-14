d052e3d0aee1ea2e67f649e66545892e01ff72e6
Valledor099 <alumno27.valledor.agustin@ipm.edu.ar>
2026-03-14 20:14:06 -0300

    Ejercicio 1 Terminado

--- ejerciciosLabo2026/src/Unidad_0/IngresoDatos.java
+++ ejerciciosLabo2026/src/Unidad_0/IngresoDatos.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class IngresoDatos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ingrese valor de N");
+        System.out.print("ingrese valor de N: ");
         int N = scanner.nextInt();
-        System.out.print("ingrese valor de A");
+        System.out.print("ingrese valor de A: ");
         double A = scanner.nextDouble();
-        System.out.print("ingrese valor de C");
-        char C =  scanner.next();
+        System.out.print("ingrese valor de C: ");
+        char C =  scanner.next().charAt(0);
         System.out.println("Variable N = " + N);
         System.out.println("Variable A = " + A);
         System.out.println("Variable C = " + C);
