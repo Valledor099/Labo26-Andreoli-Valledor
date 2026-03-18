dd8e0170afc62056ddb1a52f52a7ac6599783295
Valledor099 <alumno27.valledor.agustin@ipm.edu.ar>
2026-03-18 09:04:23 -0300

    agregamos el while a el DivisibleFor/While

--- ejerciciosLabo2026/src/Unidad_0/DivisibleFor.java
+++ ejerciciosLabo2026/src/Unidad_0/DivisibleFor.java
@@ -3,6 +3,7 @@ package Unidad_0;
 public class DivisibleFor {
     public static void main(String[] args) {
         System.out.println("Los numeros divisibles por 2 y 3 son: ");
+        int j = 0;
 
         for(int i = 1; i <= 100; i++)
         {
@@ -10,6 +11,17 @@ public class DivisibleFor {
             {
                 System.out.println(i);
             }
+        }
+
+        System.out.println("Ahora con while: ");
+
+        while(j <= 100)
+        {
+            if(j % 6 == 0)
+            {
+                System.out.println(j);
+            }
+        j++;
         }
     }
 }
