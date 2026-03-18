5c2d50f9afac9f47333e3525f544bec085dc4a3d
Valledor099 <alumno27.valledor.agustin@ipm.edu.ar>
2026-03-18 09:33:55 -0300

    Ejercicio 8 terminado

--- ejerciciosLabo2026/src/Unidad_0/IntroducirNumeros.java
+++ ejerciciosLabo2026/src/Unidad_0/IntroducirNumeros.java
@@ -3,23 +3,11 @@ package Unidad_0;
 import java.util.Scanner;
 
 public class IntroducirNumeros {
-    public static int positive(int num)
-    {
-        int positivos = 0, aux = 0;
-
-        if (num < aux) {
-            positivos += num;
-        }
-
-        return positivos;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese un numero, -1 para salir");
         int n = scanner.nextInt() , cont= 0, max = 0, min = 0, total=0;
-        int sumPositivos = 0;
-        int negativos = 0 , aux = 0;
+        int negativos = 0 , aux = 0, positivos = 0;;
 
         while(n != -1)
         {
@@ -38,12 +26,22 @@ public class IntroducirNumeros {
                 min = n;
             }
 
-            sumPositivos = positive(n);
-            
+            if (n > aux){
+                positivos+=n;
+            }
+
+            if (n < aux)
+            {
+                negativos+=n;
+            }
 
         }
 
         System.out.println("Cantidad de numeros introducidos: " + cont);
-
+        System.out.println("Mayor numero: " + max);
+        System.out.println("Menor numero: " + min);
+        System.out.println("Suma de todos los numeros: " + total);
+        System.out.println("Suma de numeros positivos: " + positivos);
+        System.out.println("Suma de numeros negativos: " + negativos);
     }
 }
