64954056ee83805ba0608cf41b96d029fa40922f
Valledor099 <alumno27.valledor.agustin@ipm.edu.ar>
2026-03-27 15:27:11 -0300

    Ej 3 terminado

--- ejerciciosLabo2026/src/Unidad_0/Coche.java
+++ ejerciciosLabo2026/src/Unidad_0/Coche.java
@@ -4,11 +4,13 @@ public class Coche {
     private String marca;
     private String modelo;
     private String color;
+    private int velocidad;
 
     public Coche(String marca, String modelo, String color){
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
+        this.velocidad = 0;
     }
 
     public String getMarca() {
@@ -34,5 +36,30 @@ public class Coche {
     public void setColor(String color){
         this.color= color;
     }
-    
+
+    public void acelerar(){
+        velocidad += 5;
+    }
+
+    public void frenar(){
+        velocidad = 0;
+    }
+
+    public int mostrarVelocidad(){
+        return velocidad;
+    }
+
+    public static void main(String[] args) {
+        Coche coche = new Coche("lamborghini","Urus", "gris");
+        System.out.println("marca, modelo, color: " + coche.getMarca() + " "+ coche.getModelo()+ " " + coche.getColor());
+        coche.setMarca("Porsche");
+        coche.setModelo("911 gt-rs 3");
+        coche.setColor("Negro");
+        System.out.println("marca, modelo, color: " + coche.getMarca() + " "+ coche.getModelo() +" "+ coche.getColor());
+        coche.acelerar();
+        System.out.println(coche.mostrarVelocidad());
+        coche.frenar();
+        System.out.println(coche.mostrarVelocidad());
+    }
+
 }
