c9e7688eb28c8e8385308f2fc72c83e2069b86d6
Thiago-1203 <alumno27.andreoli.castro.thiago@ipm.edu.ar>
2026-05-08 13:59:58 -0300

    Ej 2 hecho

--- ejerciciosLabo2026/src/vehiculos/Empresa.java
+++ ejerciciosLabo2026/src/vehiculos/Empresa.java
@@ -1,12 +1,13 @@
 package vehiculos;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Empresa {
     private ArrayList<Vehiculo>Lista;
 
-    public Empresa(ArrayList<Vehiculo> lista) {
-        Lista = lista;
+    public Empresa() {
+        Lista = new ArrayList<>();
     }
 
     public ArrayList<Vehiculo> getLista() {
@@ -18,27 +19,30 @@ public class Empresa {
     }
 
 public void agregarVehiculo(Vehiculo vehiculo){
+        boolean esIgual= false;
+
+        if (Lista.isEmpty()){
+        Lista.add(vehiculo);}
+
+        else if (vehiculo.getClass() == Bicicleta.class){
+            Lista.add(vehiculo);
+        }
+
+        else {
 
         for (Vehiculo v1 : Lista) {
 
-            if (v1.getClass() == Coche.class){
-                if (!vehiculo.getPatente().equals(v1.getPatente())){
-                    Lista.add(vehiculo);
+                if (v1.getPatente().equals(vehiculo.getPatente())){
+                        esIgual = true;
+                        break;
                 }
-            }
-
-            else if (v1.getClass() == Camioneta.class) {
-                if (!vehiculo.getPatente().equals(v1.getPatente())){
-                    Lista.add(vehiculo);
-                }
-            }
-
-            else{
-                Lista.add(vehiculo);
-            }
         }
 
+        if (!esIgual){
+            Lista.add(vehiculo);
+        }
 
+        }
 }
 
 public String masTipo(){
@@ -74,7 +78,61 @@ public String masTipo(){
     return tipo;
 }
 
-public void cargarCamioneta(int carga, int camioneta){
+public void cargarCamioneta(int carga, String camioneta){
+
+    for (Vehiculo v1 : Lista) {
+
+        if (v1.getClass() == Camioneta.class) {
+            if (v1.getPatente().equals(camioneta)){
+                if (((Camioneta) v1).getCapacidad() > carga){
+                    ((Camioneta) v1).setCapacidad(((Camioneta) v1).getCapacidad()-carga);
+                }
+            }
+        }
+
+    }
+
+
 
 }
+
+public int cantDescapotable(){
+
+  int cont = 0;
+
+  for (Vehiculo v1 : Lista) {
+        if (v1.getClass()== Coche.class){
+            if (((Coche) v1).isDescapotable())
+                cont+=1;
+            }
+  }
+
+  return cont;
+  }
+
+
+
+
+public static void main(String[] args) {
+
+    Camioneta camion = new Camioneta("Toyota","Hilux","Rojo",4, LocalDate.of(1999,10,10),"HJK-192",100);
+    Coche auto = new Coche("Porsche","Cayenne","Negro",4,LocalDate.of(2020,10,1),"AF-123-KD",false);
+    Coche auto1 = new Coche("Porsche","Cayenne","Negro",4,LocalDate.of(2020,10,1),"AF-123-KD",false);
+    Coche auto2 = new Coche("Porsche","Cayenne","Gris",4,LocalDate.of(2022,10,4),"FA-432-HA",true);
+    Bicicleta bicicleta = new Bicicleta("BMX","freestyle","Azul",2,LocalDate.of(2026,1,1));
+
+    Empresa empresa = new Empresa();
+
+    empresa.agregarVehiculo(camion);
+    empresa.agregarVehiculo(auto);
+    empresa.agregarVehiculo(auto1);
+    empresa.agregarVehiculo(auto2);
+    empresa.agregarVehiculo(bicicleta);
+
+    System.out.println("Mas vehiculos en la empresa:" + empresa.masTipo() );
+    empresa.cargarCamioneta(150,"HJK-192");
+    System.out.println("Cantidad de descapotables:" + empresa.cantDescapotable());
+
+}
+
 }
